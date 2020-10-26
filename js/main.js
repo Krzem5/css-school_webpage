dcb0d5cb0383c4e38ddd674390d6f7d0630c4530
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-26 21:03:38 +0100

    Push Update 10/26/2020, 21:03:29

--- js/main.js
+++ js/main.js
@@ -13,14 +13,13 @@ const SIDEBAR_DATA=[
 
 document.addEventListener("DOMContentLoaded",()=>{
 	if (!/^https?:\/\/[a-zA-Z0-9]+\.github\.io\//.test(window.location.href)){
-		console.log(window.location.href);
 		// window.location.href="https://krzem5.github.io/Css-School_Webpage/";
 	}
 	document.querySelectorAll(".bg .wr .top .icon")[0].onclick=()=>{
-		window.location.href="/";
+		window.location.href="/Css-School_Webpage";
 	};
 	let se=document.querySelectorAll(".bg .wr .side")[0];
 	for (let k of SIDEBAR_DATA){
-		se.innerHTML+=`<div class="elem" onclick="document.location.href='${k.url}'">${k.nm}</div>`;
+		se.innerHTML+=`<div class="elem" onclick="document.location.href='/Css-School_Webpage/${k.url}'">${k.nm}</div>`;
 	}
 },false);
