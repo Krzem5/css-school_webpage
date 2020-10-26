f12eddc3ea0fe593e9ad56160bc8b14d131d7a15
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-26 21:01:03 +0100

    Push Update 10/26/2020, 21:00:54

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+const SIDEBAR_DATA=[
+	{
+		"nm": "How to Code, Part I",
+		"url": "/how-to-code.html"
+	},
+	{
+		"nm": "How to Code, Part II",
+		"url": "/how-to-code-2.html"
+	}
+];
+
+
+
 document.addEventListener("DOMContentLoaded",()=>{
-	if (!window.location.href.startsWith("https://krzem5.github.io/Css-School_Webpage/")){
+	if (!/^https?:\/\/[a-zA-Z0-9]+\.github\.io\//.test(window.location.href)){
 		console.log(window.location.href);
 		// window.location.href="https://krzem5.github.io/Css-School_Webpage/";
 	}
-	document.querySelectorAll(".bg .wr .top .icon").onclick=()=>{
+	document.querySelectorAll(".bg .wr .top .icon")[0].onclick=()=>{
 		window.location.href="/";
 	};
+	let se=document.querySelectorAll(".bg .wr .side")[0];
+	for (let k of SIDEBAR_DATA){
+		se.innerHTML+=`<div class="elem" onclick="document.location.href='${k.url}'">${k.nm}</div>`;
+	}
 },false);
