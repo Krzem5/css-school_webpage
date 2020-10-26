c2c042c083789208d94250f7649ce17d5d03b5c9
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-26 20:26:31 +0100

    Push Update 10/26/2020, 20:26:25

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
+	if (window.location.href.substring(44)!="https://krzem5.github.io/Css-School_Webpage/"){
+		window.location.href="https://krzem5.github.io/Css-School_Webpage/";
+	}
 	document.querySelectorAll(".bg .wr .top .icon").onclick=()=>{
 		window.location.href="/";
 	};
