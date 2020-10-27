db750401e1a0a1eec8eb80f56d9e18d48575c64e
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-27 16:39:13 +0100

    Push Update 10/27/2020, 16:39:02

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const SIDEBAR_DATA=[
 	{
 		"nm": "How to Code, Part I",
-		"url": "/how-to-code.html"
+		"url": "how-to-code.html"
 	},
 	{
 		"nm": "How to Code, Part II",
-		"url": "/how-to-code-2.html"
+		"url": "how-to-code-2.html"
 	}
 ];
 
@@ -13,13 +13,23 @@ const SIDEBAR_DATA=[
 
 document.addEventListener("DOMContentLoaded",()=>{
 	if (!/^https?:\/\/[a-zA-Z0-9]+\.github\.io\//.test(window.location.href)){
-		window.location.href="https://krzem5.github.io/Css-School_Webpage/";
+		// window.location.href="https://krzem5.github.io/Css-School_Webpage/";
 	}
 	document.querySelectorAll(".bg .wr .top .icon")[0].onclick=()=>{
 		window.location.href="/Css-School_Webpage";
 	};
+	let te=document.querySelectorAll(".bg .wr .top .title")[0];
+	te.innerHTML=te.innerText.split("").map((e)=>{
+		return `<span class="c">${e}</span>`;
+	}).join("");
 	let se=document.querySelectorAll(".bg .wr .side")[0];
 	for (let k of SIDEBAR_DATA){
-		se.innerHTML+=`<div class="elem" onclick="document.location.href='/Css-School_Webpage${(!/^[\\\/]/.test(k.url[0])?"/":"")+k.url}'">${k.nm}</div>`;
+		se.innerHTML+=`<div class="elem" onclick="window._load('${k.url}'">${k.nm}</div>`;
 	}
+	window._load=(u)=>{
+		fetch(`/Css-School_Webpage${(!/^[\\\/]/.test(u[0])?"/":"")+u}`).catch((e)=>console.log(e)).then((e)=>e.text()).then((txt)=>{
+			console.log(txt);
+		});
+	}
+	window._load("main.html");
 },false);
