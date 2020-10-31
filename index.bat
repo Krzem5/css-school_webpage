5f675c5360345d4fdf06a3df1cfe3c28dc913420
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-10-31 13:49:17 +0100

    Push Update 10/31/2020, 13:45:37

--- index.bat
+++ index.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-"C:/Program Files/Google/Chrome Dev/Application/chrome.exe" http://localhost:8020/D:\K\Coding\projects\Css-School_Webpage\
+"C:/Program Files/Google/Chrome Dev/Application/chrome.exe" http://localhost:8020/D\K\Coding\Css-School_Webpage\
